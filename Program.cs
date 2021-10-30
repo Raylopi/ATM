e75e6ac88a5fe9b580983ee6e33a145f851432bd
Raylopi <raimondo.lopilato@gmail.com>
2021-10-30 20:11:35 +0200

    commit on master

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+This simple project will essentially create a simulation of an ATM within a Windows program. Just like an ATM, the program should have at least the following features:
+
+    Checking whether an input – such as an ATM card (a debit/credit card number) – is recorded correctly
+    Verifying the user by asking for a PIN
+    In case of negative verification, logging out the user
+    In case of positive verification, showing multiple options, including cash availability, the previous five transactions, and cash withdrawal
+    Giving the user the ability to withdraw up to $1,000 worth of cash in one transaction, with total transactions limited to ten per day.
+
+For a more complicated program, include the ability to register a new PIN and mobile number, a detailed bank statement, and a “fast” cash withdrawal system for quickly withdrawing $20, $50, or $100.
+*/
+
+using System;
+
+namespace ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
